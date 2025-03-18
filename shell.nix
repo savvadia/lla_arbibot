f32543092abde9992e6096b0aa7fe48ab14f37d5
Diana Savvatina <diana.savvatina@gmail.com>
2025-03-18 12:55:48 +0000

    env: branch in prompt

--- shell.nix
+++ shell.nix
@@ -14,6 +14,6 @@ pkgs.mkShell {
         source ~/.bash_profile
     fi
     echo "Nix-shell started..."
-    export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]nix:\[\033[33;1m\]\w\[\033[m\]> "
+    export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]nix:\[\033[33;1m\]\w\[\033[m\]:\[\033[31;1m\]\$(parse_git_branch)\[\033[m\]\$ "
   '';
 }
